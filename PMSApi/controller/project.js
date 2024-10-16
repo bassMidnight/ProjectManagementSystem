a95c8d13c6d7d358b7429661b65742172bb4e87b
Oligooosite <sanpong94@gmail.com>
2024-10-16 16:09:43 +0700

    catch Projectid in DeleteProject

--- PMSApi/controller/project.js
+++ PMSApi/controller/project.js
@@ -121,6 +121,9 @@ async function DeleteProject(req, res) {
     }
     try {
         const deletedProject = await Project.findOneAndDelete({id});
+        if (!deletedProject) {
+            return res.status(404).json({error: true, message: 'Project not found' });
+        }
         return res.status(200).json({error: false, message: 'Delete success', data: deletedProject });
     } catch (error) {
         return res.status(500).json({error: true, message: error.message });
